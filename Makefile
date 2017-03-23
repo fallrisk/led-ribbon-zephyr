d549b72721bacadd0e70271a48fb6ff0f2f12747
Justin Watson <jwatson5@gmail.com>
2017-03-23 15:52:55 -0700

    Added basic project files.
    Followed the information at https://www.zephyrproject.org/doc/application/application.html.

---
+++ Makefile
@@ -0,0 +1,4 @@
+BOARD = atmel_sam4s
+CONF_FILE ?= prj.conf
+
+include ${ZEPHYR_BASE}/Makefile.inc
---
+++ src/led-ribbon-zephyr.c
@@ -0,0 +1,8 @@
+
+#include <zephyr.h>
+
+void main(void)
+{
+	// Connect to the SPI device.
+	// Send a ribbon packet out the SPI.
+}
